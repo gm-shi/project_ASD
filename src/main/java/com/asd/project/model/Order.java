19c8cb9631f6671f789fac46a51390f1a68d7742
WZF888888 <100253117+WZF888888@users.noreply.github.com>
2022-10-02 08:34:43 +0800

    Rev0.0.11 Getters Setters Constructor

--- src/main/java/com/asd/project/model/Order.java
+++ src/main/java/com/asd/project/model/Order.java
@@ -1,4 +1,49 @@
 package com.asd.project.model;
 
+import java.util.Date;
+
 public class Order {
+    private int OrderID;
+    private int Quantity;
+    private String DishName;
+    private Date OrderDate;
+
+    public Order(int orderID, int quantity, String dishName, Date orderDate) {
+        OrderID = orderID;
+        Quantity = quantity;
+        DishName = dishName;
+        OrderDate = orderDate;
+    }
+
+    public int getOrderID() {
+        return OrderID;
+    }
+
+    public void setOrderID(int orderID) {
+        OrderID = orderID;
+    }
+
+    public int getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        Quantity = quantity;
+    }
+
+    public String getDishName() {
+        return DishName;
+    }
+
+    public void setDishName(String dishName) {
+        DishName = dishName;
+    }
+
+    public Date getOrderDate() {
+        return OrderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        OrderDate = orderDate;
+    }
 }
