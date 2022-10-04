6d42b84f7ec3ddcc5c207631238f100fa68e8846
WZF888888 <100253117+WZF888888@users.noreply.github.com>
2022-10-05 06:30:46 +0800

    Rev1.1.0.4 OrderTest

---
+++ src/test/java/com/asd/project/model/OrderTest.java
@@ -0,0 +1,44 @@
+package com.asd.project.model;
+
+import com.asd.project.model.dao.OrderDao;
+import com.asd.project.utils.DB;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+
+import java.sql.SQLException;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class OrderTest{
+    DB db;
+    OrderDao orderDao = new OrderDao(new DB());
+    public OrderTest() throws SQLException {
+
+    }
+
+    @Test
+    public void addquantitytest() throws SQLException {
+        orderDao.addquantity(92,1);
+    }
+
+    @Test
+    public void deletedish() throws SQLException {
+        orderDao.deletedish(92,1);
+    }
+
+    @Test
+    public void viewcarttest() throws SQLException{
+        orderDao.viewcart(92);
+    }
+
+    @Test
+    public void minsquantitytest() throws SQLException{
+        orderDao.minusquantity(92,1);
+    }
+
+    @Test void submittest() throws SQLException{
+        orderDao.submitorder(92);
+    }
+
+
+}
