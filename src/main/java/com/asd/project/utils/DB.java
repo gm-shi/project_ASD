dc47e3bf44f32e13af9fee176ef4bc84fc67059c
Gongming <gm.shi@outlook.com>
2022-10-12 10:35:34 +1100

    setup env for db

--- src/main/java/com/asd/project/utils/DB.java
+++ src/main/java/com/asd/project/utils/DB.java
@@ -5,21 +5,25 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DB {
-    protected final String URL = "jdbc:mysql://database-asd-project.cjsubjzglg6d.ap-southeast-2.rds.amazonaws.com:3306/";
-    protected  String db = "ASD_DB";
-    protected  String dbUser = "admin";
-    protected  String dbPassword = "123456789";
+    protected final String URL = System.getenv("DB_URL");
+    protected  String db = System.getenv("DB_NAME");
+    protected  String dbUser = System.getenv("DB_USER");
+    protected  String dbPassword = System.getenv("DB_PASSWORD");
     protected String dbDriver = "com.mysql.cj.jdbc.Driver";
     protected Connection conn;
 
     public DB() throws SQLException {
+        System.out.println(URL);
+        System.out.println(db);
+        System.out.println(dbUser);
+        System.out.println(dbPassword);
+
         try {
             Class.forName(this.dbDriver).newInstance();
         } catch (Exception e) {
             e.printStackTrace();
         }
         this.conn = DriverManager.getConnection(URL + db + "?serverTimezone=Australia/Sydney", dbUser, dbPassword);
-
     }
 
     public Connection connection() throws SQLException {
